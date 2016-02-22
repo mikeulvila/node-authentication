a7cc842f6bd7e76ed9a796ed387203d4d79e696c
Mike Ulvila <mikeulvila@me.com>
2016-02-22 09:48:51 -0600

    login view

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,10 +11,27 @@ const PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  res.redirect('/');
+});
+
+// server listen
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
