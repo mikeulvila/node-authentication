3936e5695fedce118ce5104c4a92f1d8d9c32d5b
Mike Ulvila <mikeulvila@me.com>
2016-02-22 11:17:21 -0600

    added express-session and redis db to save user.sid

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'supersecret';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +17,21 @@ app.set('view engine', 'jade');
 // body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// express-session middleware
+app.use(session({
+  store: new RedisStore(),
+  secret: SESSION_SECRET
+}));
+// create own middleware
+app.use((req, res, next) => {
+  req.session.visits = req.session.visits || {};
+  req.session.visits[req.url] = req.session.visits[req.url] || 0;
+  req.session.visits[req.url]++;
+
+  console.log(req.session);
+  next();
+});
 
 // set public folder
 app.use(express.static(path.join(__dirname, 'public')));
